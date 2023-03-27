4117d3460e24279d1e9b295efb7cba6313bbe653
leash <leash2480@gmail.com>
2023-03-27 17:13:25 +0300

    point and vector

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -44,4 +44,38 @@ public class Point {
     public String toString() {
         return point3d.toString();
     }
+
+    /**
+     *Subtraction vector and dot
+     * @param point to subtract
+     * @return Vector
+     */
+    public Vector subtract(Point point) {
+        return new Vector(point3d.subtract(point.point3d));
+    }
+    /**
+     *Connecting vector and dot
+     * @param vec to adding
+     * @return point
+     */
+    public Point add(Vector vec) {
+        return new Point(this.point3d.add(vec.point3d));
+    }
+    /**
+     *Distance squared
+     * @param point
+     * @return Distance squared
+     */
+    public double distanceSquared(Point point) {
+        Double3 result =this.point3d.subtract(point.point3d);
+        return (result.d1*result.d1)+(result.d2*result.d2)+(result.d3*result.d3);
+    }
+    /**
+     *Distance
+     * @param point
+     * @return Distance
+     */
+    public double distance(Point point) {
+        return Math.sqrt(this.distanceSquared(point));
+    }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -28,4 +28,54 @@ public class Vector extends Point {
     public Vector normalize(){
         return new Vector(point3d.reduce(length()));
     }
+    /**
+     * add two vectors
+     * @param vec the other vector to add
+     * @return new Vector from (x1 + x2, y1 + y2, z1 + z2)
+     */
+    public Vector add(Vector vec) {
+        return new Vector(point3d.add(vec.point3d));
+    }
+    /**
+     * multiply this vector with a scalar
+     * @param num the scalar
+     * @return new Vector with the value of (scalar * x, scalar * y, scalar * z)
+     * @throws IllegalArgumentException if scalar = 0
+     */
+    public Vector scale (double num){
+        if (num==0)
+            throw new IllegalArgumentException("vector 0");
+        return new Vector(point3d.d1*num, point3d.d2*num, point3d.d3*num);
+    }
+    /**
+     * dot product of tow vectors (x1, y1, z1), (x2, y2, z2)
+     * @param vec (x2, y2, z2)
+     * @return x1*x2 + y1*y2 + z1*z2
+     */
+    public double dotProduct(Vector vec){
+        return (vec.point3d.d1 * point3d.d1)+(vec.point3d.d2 * point3d.d2)+(vec.point3d.d3 * point3d.d3);
+    }
+    /**
+     * cross product of two vectors (x1, y1, z1), (x2, y2, z2)
+     * @param vec (x2, y2, z2)
+     * @return new Vector from (x1, y1, z1)X(x2, y2, z2) =
+     * = (y1*z2 -z1*y2, z1*x2 - x1*z2, x1*y2 - y1*x2)
+     */
+    public Vector crossProduct(Vector vec){
+        return new Vector(point3d.d2*vec.point3d.d3-point3d.d3*vec.point3d.d2, point3d.d3*vec.point3d.d1- point3d.d1*vec.point3d.d3, point3d.d1*vec.point3d.d2- point3d.d2*vec.point3d.d1);
+    }
+    /**
+     * the squared length of the vector (x, y, z)
+     * @return x^2 + y^2 + z^2
+     */
+    public double lengthSquared(){
+        return (point3d.d1 * point3d.d1)+(point3d.d2 * point3d.d2)+(point3d.d3 * point3d.d3);
+    }
+    /**
+     * the length of the vector (x, y, z)
+     * @return sqrt(x^2 + y^2 + z^2)
+     */
+    public double length(){
+        return Math.sqrt(this.lengthSquared());
+    }
 }
