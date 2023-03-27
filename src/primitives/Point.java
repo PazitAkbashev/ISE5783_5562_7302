029ecf21f57f6ef0a548dc8247b12bffebb04712
Pazit Akbashev <pakbashev@gmail.com>
2023-03-27 14:07:55 +0300

    adding comments

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,18 +1,30 @@
 package primitives;
 import primitives.Double3;
-
 import java.util.Objects;
 
+/**
+ * this is a basic point for RayTracing project in 3D
+ * @author Pazit and Leah
+ */
 public class Point {
-    private Double3 point3d;
+    protected Double3 point3d;
 
+    /**
+     * constructor
+     * @param point3d value of point
+     */
     public Point(Double3 point3d) {
         this.point3d = point3d;
     }
 
-
+    /**
+     * constructor
+     * @param d1 value of x
+     * @param d2 value of y
+     * @param d3 value of z
+     */
      public Point (double d1, double d2, double d3){
-         this.point3d = new Double3(d1, d2, d3);
+        this.point3d = new Double3(d1, d2, d3);
     }
 
     @Override
@@ -26,5 +38,10 @@ public class Point {
     @Override
     public int hashCode() {
         return Objects.hash(point3d);
+    }
+
+    @Override
+    public String toString() {
+        return point3d.toString();
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,15 +1,31 @@
 package primitives;
 
+/**
+ * this class represent a Vector of 3D
+ * @author Pazit and Leah
+ */
 public class Vector extends Point {
+    /**
+     * @param point3d is a 3d point
+     */
     public Vector(Double3 point3d) {
         super(point3d);
     }
 
+    /**
+     * @param d1 value of x
+     * @param d2 value of y
+     * @param d3 value of z
+     */
     public Vector(double d1, double d2, double d3) {
         super(d1, d2, d3);
     }
 
+    /**
+     *the normal of this vector of (x, y, z)
+     * @return (x, y, z) / |(x, y, z)| = (x, y, z) / sqrt(x^2 + y^2 + z^2)
+     */
     public Vector normalize(){
-        return new Vector(xyz.reduce(length()));
+        return new Vector(point3d.reduce(length()));
     }
 }
