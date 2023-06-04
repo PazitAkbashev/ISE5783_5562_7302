0c1b430d38ddecd2922e036a4c2a34c258ccd3d5
Pazit Akbashev <pakbashev@gmail.com>
2023-06-04 17:04:31 +0300

    bdika bdika

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -37,7 +37,7 @@ public class RayTracerBasic extends RayTracerBase {
         Ray lightRay = new Ray(point, lightDirection);
         List<GeoPoint> intersections = scene.geometries.findGeoIntersections(lightRay);
         return intersections.isEmpty(); //TODO
-        //return bdika
+        //return bdika bdika
     }
 
 //        Vector lightDirection = l.scale(-1); // from point to light source
